9278eda88c15fbaf232fa887252902a28b95f190
Karl Duderstadt <duderstadt@biochem.mpg.de>
2023-05-29 09:09:41 +0200

    ImageFormatter: channel stack support

--- src/main/java/de/mpg/biochem/mars/kymograph/ImageFormatter.java
+++ src/main/java/de/mpg/biochem/mars/kymograph/ImageFormatter.java
@@ -37,11 +37,8 @@ public class ImageFormatter {
     private final Map<Integer, Double> displayRangeCtoMin, displayRangeCtoMax;
     private final Map<Integer, String> cToLUTName;
     private String htmlEncodedImage;
-    private int leftRightMargin = 50;
-    private int topBottomMargin = 50;
-
-    private int fullWidth, fullHeight, imgPixelWidth, imgPixelHeight;
-
+    private int horizontalMargin = 50;
+    private int verticalMargin = 50;
     private boolean showXAxis = true;
     private String xAxisLabel = "";
 
@@ -63,6 +60,7 @@ public class ImageFormatter {
     private double yMaxValue = 0;
     
     private int channelStackspacing = 10;
+    private boolean channelStack = false;
     private String title = "";
 
     private int xaxis_precision = 0;
@@ -141,17 +139,18 @@ public class ImageFormatter {
     }
 
     public ImageFormatter setHorizontalMargin(int horizontalMargin) {
-        this.leftRightMargin = horizontalMargin;
+        this.horizontalMargin = horizontalMargin;
         return this;
     }
 
     public ImageFormatter setVerticalMargin(int verticalMargin) {
-        this.topBottomMargin = verticalMargin;
+        this.verticalMargin = verticalMargin;
         return this;
     }
 
     public ImageFormatter showChannelStack(int spacing) {
         this.channelStackspacing = spacing;
+        this.channelStack = true;
         return this;
     }
 
@@ -178,47 +177,44 @@ public class ImageFormatter {
     public void build() {
         for (int c=1; c<=imp.getNChannels(); c++) {
             imp.setC(c);
-            if (displayRangeCtoMin.containsKey(c) || displayRangeCtoMax.containsKey(c))
-                imp.setDisplayRange(displayRangeCtoMin.containsKey(c) ? displayRangeCtoMin.get(c) : imp.getDisplayRangeMin(),
-                        displayRangeCtoMax.containsKey(c) ? displayRangeCtoMax.get(c) : imp.getDisplayRangeMax());
-            else IJ.run(imp, "Enhance Contrast", "saturated=0.35");
-            if (cToLUTName.containsKey(c))
-                IJ.run(imp, cToLUTName.get(c), "");
-            else if (c == 1)
-                IJ.run(imp, "Blue", "");
-            else if (c == 2)
-                IJ.run(imp, "Magenta", "");
-            else if (c == 3)
-                IJ.run(imp, "Green", "");
+            updateChannelColor(imp, c);
         }
         imp = imp.resize(imp.getWidth()*rescaleFactor, imp.getHeight()*rescaleFactor, 1, "none");
 
-        fullWidth = imp.getWidth() + leftRightMargin*2;
-        fullHeight = imp.getHeight() + topBottomMargin*2;
+        int fullWidth = imp.getWidth() + horizontalMargin*2;
+        int fullHeight = (channelStack) ? (1 + imp.getNChannels())*imp.getHeight() + imp.getNChannels()*channelStackspacing + verticalMargin*2 : imp.getHeight() + verticalMargin*2;
 
         BufferedImage image = new BufferedImage(fullWidth, fullHeight, BufferedImage.TYPE_INT_ARGB);
         Graphics g = image.createGraphics();
 
-        //Draw composite image
-        IJ.run(imp, "Make Composite", "");
-        g.drawImage(imp.getBufferedImage(), leftRightMargin, topBottomMargin, null);
-
-        imgPixelWidth = fullWidth - leftRightMargin * 2;
-        imgPixelHeight = fullHeight - topBottomMargin * 2;
-
         bounds = new Rectangle2D.Double(xMinValue, yMinValue, xMaxValue - xMinValue, yMaxValue - yMinValue);
 
         transform = new AffineTransform();
-        transform.translate(leftRightMargin, topBottomMargin + imgPixelHeight);
-        transform.scale(imgPixelWidth / bounds.width, -imgPixelHeight / bounds.height);
+        transform.translate(horizontalMargin, verticalMargin + imp.getHeight());
+        transform.scale(imp.getWidth() / bounds.width, -imp.getHeight() / bounds.height);
         transform.translate(-bounds.x, -bounds.y);
 
-        if (showXAxis) paintXAxis(g);
-        if (showYAxis) paintYAxis(g);
+        g.drawImage(imp.getBufferedImage(), horizontalMargin, verticalMargin, null);
+        if (showYAxis) paintYAxis(g, verticalMargin, verticalMargin + imp.getHeight());
+        if (showXAxis) paintXAxis(g, imp.getWidth(), fullHeight - verticalMargin*2);
+
+        if (channelStack)
+            for (int c=1; c<=imp.getNChannels(); c++) {
+                transform = new AffineTransform();
+                transform.translate(horizontalMargin, verticalMargin + (c + 1)*imp.getHeight() + c*channelStackspacing);
+                transform.scale(imp.getWidth() / bounds.width, -imp.getHeight() / bounds.height);
+                transform.translate(-bounds.x, -bounds.y);
+
+                imp.setC(c);
+                ImagePlus temp = new ImagePlus("temp image", imp.getChannelProcessor());
+                updateChannelColor(temp, c);
+                g.drawImage(temp.getBufferedImage(), horizontalMargin, verticalMargin + c*(imp.getHeight() + channelStackspacing), null);
+                if (showYAxis) paintYAxis(g, verticalMargin + c*imp.getHeight() + c*channelStackspacing, verticalMargin + (c + 1)*imp.getHeight() + c*channelStackspacing);
+            }
+
         //add title
         g.setFont(title_font);
-        ((Graphics2D) g).drawString(title, leftRightMargin + imgPixelWidth / 2 - g.getFontMetrics().stringWidth(title) / 2, g.getFontMetrics(title_font).getHeight());
-
+        ((Graphics2D) g).drawString(title, horizontalMargin + imp.getWidth() / 2 - g.getFontMetrics().stringWidth(title) / 2, g.getFontMetrics(title_font).getHeight());
         g.dispose();
 
         try {
@@ -232,7 +228,22 @@ public class ImageFormatter {
         }
     }
 
-    private void paintXAxis(Graphics g) {
+    private void updateChannelColor(ImagePlus img, int c) {
+        if (displayRangeCtoMin.containsKey(c) || displayRangeCtoMax.containsKey(c))
+            img.setDisplayRange(displayRangeCtoMin.containsKey(c) ? displayRangeCtoMin.get(c) : img.getDisplayRangeMin(),
+                    displayRangeCtoMax.containsKey(c) ? displayRangeCtoMax.get(c) : img.getDisplayRangeMax());
+        else IJ.run(img, "Enhance Contrast", "saturated=0.35");
+        if (cToLUTName.containsKey(c))
+            IJ.run(img, cToLUTName.get(c), "");
+        else if (c == 1)
+            IJ.run(img, "Blue", "");
+        else if (c == 2)
+            IJ.run(img, "Magenta", "");
+        else if (c == 3)
+            IJ.run(img, "Green", "");
+    }
+
+    private void paintXAxis(Graphics g, int imgPixelWidth, int imgPixelHeight) {
         Graphics2D g2d = (Graphics2D) g;
         g2d.setStroke(new BasicStroke(2.0f));
 
@@ -243,18 +254,19 @@ public class ImageFormatter {
             Point2D.Double p = new Point2D.Double(x, 0);
             transform.transform(p, p);
 
-            if (p.x >= leftRightMargin) {
+            if (p.x >= horizontalMargin) {
                 g2d.setColor(Color.BLACK);
-                g2d.drawLine((int)p.x, topBottomMargin + imgPixelHeight, (int)p.x, topBottomMargin + imgPixelHeight + 5);
-                g2d.drawString(String.format("%." + xaxis_precision + "f", x), (int)p.x - g.getFontMetrics(font).stringWidth(String.format("%." + xaxis_precision + "f", x))/2, topBottomMargin + imgPixelHeight + g.getFontMetrics(font).getHeight() + 2);
+                g2d.drawLine((int)p.x, verticalMargin + imgPixelHeight, (int)p.x, verticalMargin + imgPixelHeight + 5);
+                g2d.drawString(String.format("%." + xaxis_precision + "f", x), (int)p.x - g.getFontMetrics(font).stringWidth(String.format("%." + xaxis_precision + "f", x))/2, verticalMargin + imgPixelHeight + g.getFontMetrics(font).getHeight() + 2);
             }
         }
 
         g.setFont(label_font);
-        g2d.drawString(xAxisLabel, leftRightMargin + imgPixelWidth / 2 - g.getFontMetrics().stringWidth(xAxisLabel) / 2, topBottomMargin + imgPixelHeight + g.getFontMetrics(label_font).getHeight() + g.getFontMetrics(font).getHeight() + 7);
+        g2d.drawString(xAxisLabel, horizontalMargin + imgPixelWidth / 2 - g.getFontMetrics().stringWidth(xAxisLabel) / 2, verticalMargin + imgPixelHeight + g.getFontMetrics(label_font).getHeight() + g.getFontMetrics(font).getHeight() + 7);
     }
 
-    private void paintYAxis(Graphics g) {
+    private void paintYAxis(Graphics g, int imgTop, int imgBottom) {
+        int imgPixelHeight = imgBottom - imgTop;
         Graphics2D g2d = (Graphics2D) g;
         g2d.setStroke(new BasicStroke(2.0f));
 
@@ -265,17 +277,17 @@ public class ImageFormatter {
             Point2D.Double p = new Point2D.Double(0, y);
             transform.transform(p, p);
 
-            if (p.y <= imgPixelHeight + topBottomMargin) {
+            if (p.y <= imgBottom) {
                 g2d.setColor(Color.BLACK);
-                g2d.drawLine(leftRightMargin - 5, (int)p.y, leftRightMargin, (int)p.y);
-                g2d.drawString(String.format("%." + yaxis_precision + "f", y), (int)leftRightMargin - g.getFontMetrics(font).stringWidth(String.format("%." + yaxis_precision + "f", y)) - 7, (int)p.y + (int)(g.getFontMetrics(font).getHeight()/3.1));
+                g2d.drawLine(horizontalMargin - 5, (int)p.y, horizontalMargin, (int)p.y);
+                g2d.drawString(String.format("%." + yaxis_precision + "f", y), (int)horizontalMargin - g.getFontMetrics(font).stringWidth(String.format("%." + yaxis_precision + "f", y)) - 7, (int)p.y + (int)(g.getFontMetrics(font).getHeight()/3.1));
             }
         }
 
         g.setFont(label_font);
 
         AffineTransform at = g2d.getTransform();
-        g2d.translate(g.getFontMetrics(label_font).getHeight(), topBottomMargin + imgPixelHeight / 2 + g.getFontMetrics().stringWidth(yAxisLabel) / 2);
+        g2d.translate(g.getFontMetrics(label_font).getHeight(), ((imgTop + imgBottom) / 2) + g.getFontMetrics().stringWidth(yAxisLabel) / 2);
         g2d.rotate(-Math.PI / 2);
         g2d.drawString(yAxisLabel, 0, 0);
         g2d.setTransform(at);
